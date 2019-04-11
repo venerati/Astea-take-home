29122ba9b0423a0546bd0e1ffe526e13c402f81c
James Marshall <jamesomarshall@gmail.com>
2019-04-10 23:33:42 -0400

    scrapped using aws and am now going to do local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,77 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
-  title = 'Astea';
+
+  testDataLocation:'assets/data.json';
+  orders = [];
+  originalOrder = [
+    {
+      "customerInformation": "NBME",
+      "creationDate": "10-1-19",
+      "dueDate": "10-20-19",
+      "listOfOrderedProd": 
+          [{
+          "product": "wands",
+          "number": 4
+          }]
+    }
+  ];
+  numberOfItemsInOrder = [1];
+  
+  constructor() {};
+
+  ngOnInit(){
+    //make call to DB to pull list of orders
+    this.setLocalStorage()
+    this.displayExistingOrders();
+  };
+
+  displayExistingOrders(){
+    console.log('display orders fired')
+    this.orders.push(JSON.parse(localStorage.getItem("orders")));
+    console.log(this.orders)
+  }
+
+  addItemToOrder(){
+    this.numberOfItemsInOrder.push(1);
+  }
+
+  buttonClicked(event) {
+    event.preventDefault();
+    console.log(event);
+  }
+  
+
+  //make a create new order
+  createNewOrder(){
+    let anotherOrder = {
+      "customerInformation": "NBME",
+      "creationDate": "10-1-19",
+      "dueDate": "10-20-19",
+      "listOfOrderedProd": 
+          [{
+          "product": "wands",
+          "number": 4
+          }]
+    }
+    this.orders[0].push(anotherOrder);
+    console.log(this.orders)
+  }
+
+
+  setLocalStorage(){
+    localStorage.setItem("orders",JSON.stringify(this.originalOrder))
+
+  }
+
+  pullOrdersFromStorage(){
+    console.log(localStorage.length);
+    console.log(localStorage.getItem("orders"))
+    localStorage.setItem("orders",JSON.stringify(this.orders))
+  }
+  
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatButtonModule, MatExpansionModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
